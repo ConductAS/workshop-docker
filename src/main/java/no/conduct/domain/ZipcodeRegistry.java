5b48bdab85210f56c20bd061d76d63f574fcbf32
Cato Olsen <cato@conduct.no>
2015-11-05 13:10:05 +0100

    Code challenge.

--- src/main/java/no/conduct/domain/ZipcodeRegistry.java
+++ src/main/java/no/conduct/domain/ZipcodeRegistry.java
@@ -1,45 +1,10 @@
 package no.conduct.domain;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 public class ZipcodeRegistry {
 
-    @Value("${zipcode.filename}")
-    private String filename;
-
-    private final Map<String, String> zipcodes = new HashMap<>();
-
-    private void loadFromFile()
-            throws IOException {
-
-        final ClassPathResource r = new ClassPathResource(filename);
-        try (BufferedReader in = new BufferedReader(new InputStreamReader(r.getInputStream(), "UTF-8"))) {
-            in.lines().forEach(line -> {
-                final String parts[] = line.split("\t");
-                zipcodes.put(parts[0], parts[1]);
-            });
-        }
-
-    }
-
-    public synchronized String getPlace(final String zipcode)
-            throws IOException {
-
-        if (zipcodes.isEmpty()) {
-            loadFromFile();
-        }
-        return zipcodes.get(zipcode);
-
-    }
+    // IMPLEMENT ME.
 
 }
--- src/main/java/no/conduct/resources/ZipcodeController.java
+++ src/main/java/no/conduct/resources/ZipcodeController.java
@@ -1,30 +1,10 @@
 package no.conduct.resources;
 
-import no.conduct.domain.ZipcodeRegistry;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ZipcodeController {
 
-    @Autowired
-    private ZipcodeRegistry registry;
-
-    @RequestMapping(
-            value = "/registrering/{zipcode}",
-            method = RequestMethod.GET,
-            produces = MediaType.TEXT_PLAIN_VALUE
-    )
-    public
-    @ResponseBody
-    String getZipcode(@PathVariable("zipcode") final String zipcode)
-            throws IOException {
-
-        return registry.getPlace(zipcode);
-
-    }
+    // IMPLEMENT ME.
 
 }
